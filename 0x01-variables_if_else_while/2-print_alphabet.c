07412f02df3702112d61ee2bd71e8c2342493fea
Sowunmi Oluwasegun Samuel <samuelsegun833@gmail.com>
2022-03-11 19:07:30 -0800

    print alhabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,29 @@
-#​include​ ​<​stdio.h​>
-/*​* 
- ​* main - main block 
- * Description: Use `putchar` function to print the alphabet in lowercase. 
+#include<stdio.h>
+/** 
+ * main - main block 
+ * Description: Use `putchar` to print lowercase and then uppercase alphabet. 
  * Return: 0 
  */ 
-int​ ​main​(​void​) 
-{ 
- ​       char​ c = ​'​a​'​; 
+int main(void)
+{
+	char c = 'a'; 
   
- ​       while​ (c <= ​'​z​'​) 
- ​       { 
- ​               putchar​(c); 
- ​               c++; 
- ​       } 
- ​       putchar​(​'​\n​'​); 
-  
- ​       ​return​ (​0​); 
+        while (c <= 'z') 
+        { 
+                putchar(c); 
+                c++; 
+        } 
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
+
