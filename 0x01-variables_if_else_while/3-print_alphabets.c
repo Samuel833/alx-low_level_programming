738b00e5a7cc0ae11f6bc18764b355ff4bf1e390
Sowunmi Oluwasegun Samuel <samuelsegun833@gmail.com>
2022-03-13 06:53:07 -0700

    correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-/** 
- * main - main block 
- * Description: Use `putchar` to print lowercase and then uppercase alphabet. 
- * Return: 0 
- */ 
+/**
+ * main - main block
+ * Description: Use `putchar` to print lowercase and then uppercase alphabet.
+ * Return: 0
+ */
 int main(void)
 {
-	char c = 'a'; 
-  
-        while (c <= 'z') 
-        { 
-                putchar(c); 
-                c++; 
-        } 
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
 
 	c = 'A';
 
