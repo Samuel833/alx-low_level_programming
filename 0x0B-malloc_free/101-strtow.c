313d2e555a458019098b38558cc15d8a8f6c7df7
Sowunmi Oluwasegun Samuel <samuelsegun833@gmail.com>
2022-04-05 17:03:54 -0700

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,7 +10,7 @@
 int count_word(char *s)
 {
 	int flag, c, w;
-
+	
 	flag = 0;
 	w = 0;
 
@@ -21,7 +21,7 @@ int count_word(char *s)
 		else if (flag == 0)
 		{
 			flag = 1;
-			w+=;
+			w++;
 		}
 	}
 
